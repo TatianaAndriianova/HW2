d6bdb6632d8fae04268221dd0d8baa93b207a3d9
Tatiana Andriianova <ivkova.tv@gmail.com>
2025-12-12 13:10:41 +0300

    amount

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int amount = 50_000;
+        int money = 20;
 
-        // Объявляете переменные для входных данных и
-        // параметров программы: одну для хранения
-        // стоимости билета, другую для хранения количества
-        // рублей для одной бонусной мили
 
-        // Рассчитываете количество бонусных миль, используя
-        // значения заведённых переменных. Ответ сохраняете в
-        // новую переменную и выводите на экран
+        System.out.println("Количество начисленных миль"  +  amount / money );
     }
-}}
+
+
+
+
+}
